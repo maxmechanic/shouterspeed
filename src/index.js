acd7dcb6e25f2cec27aebcecf081393ffe74669b
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2016-11-19 13:39:56 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const gumPromise = navigator.mediaDevices.getUserMedia({audio: true, video: false})
+
+gumPromise.then(stream => {
+  const vids = Array.from(document.querySelectorAll('video'))
+  const context = new AudioContext()
+  const input = context.createMediaStreamSource(stream)
+  const analyser = context.createAnalyser()
+  input.connect(analyser)
+
+  const bufferLength = analyser.frequencyBinCount
+  let dataArray = new Uint8Array(bufferLength)
+  analyser.getByteTimeDomainData(dataArray)
+
+  function update() {
+    analyser.getByteFrequencyData(dataArray)
+
+    const peakFreq = Math.max(...dataArray)
+    const rate = peakFreq / 200
+
+    for (let vid of vids) {
+      vid.playbackRate = rate
+    }
+
+    requestAnimationFrame(update)
+  }
+
+  requestAnimationFrame(update)
+})
